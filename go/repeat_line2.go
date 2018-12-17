04573af9f04afb5ac324296491411a15c361272c
gaojiasheng <gaojiasheng.him@foxmail.com>
2018-12-17 18:31:23 +0800

    reader

---
+++ go/repeat_line2.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+/*
+go run repeat_line2.go < ./data/repeat_line.data
+go run repeat_line2.go ./data/repeat_line.data
+*/
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "open file error :%+v\n", err)
+			}
+			countLines(f, counts)
+		}
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
